e0ce77a52cd6cf2739262b9c90af9f7577d5fb00
David Muir Sharnoff <muir@idiom.org>
2013-03-20 17:05:38 -0700

    Revise license yet again.

--- lib.old/Text/Wrap.pm
+++ lib.old/Text/Wrap.pm
@@ -279,5 +279,5 @@ many many others.
 Copyright (C) 1996-2009 David Muir Sharnoff.  
 Copyright (C) 2012 Google, Inc.
 This module may be modified, used, copied, and redistributed at your own risk.
-Publicly redistributed modified versions that use the original name
-must pass the original test suite.
+Please do not publicly redistribute modified versions of this code with the name
+"Text::Wrap" unless it passes the unmodified Text::Wrap test suite.
